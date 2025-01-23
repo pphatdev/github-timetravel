e9ee88b88c9ddbb9d58324ac5364d3e83058064b
PPhat DEv <leatsophat02@gmail.com>
2025-01-23 22:57:31 +0700

    Update hidden.sh

--- hidden.sh
+++ hidden.sh
@@ -21,8 +21,8 @@ _() {
     git add .
 
     # Set commit dates and create commit
-    GIT_AUTHOR_DATE="${YEAR}-10-15T18:00:00" \
-    GIT_COMMITTER_DATE="${YEAR}-10-10T15:00:00" \
+    GIT_AUTHOR_DATE="${YEAR}-01-15T18:00:00" \
+    GIT_COMMITTER_DATE="${YEAR}-01-10T15:00:00" \
     git commit -m "Missed!"
 
     # Setup remote and push
